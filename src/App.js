1ecbf068691dbc9987b8a7e5040fbc718033e72a
collins <collinsleonkimani@outlook.com>
2023-06-18 16:48:47 +0300

    update.every

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   const [cards, setCards] = useState([]);
+  const [recipientInput, setRecipientInput] = useState('');
+  const defaultMessage = "Happy Father's Day to you and may you enjoy your day";
 
   useEffect(() => {
     fetch("https://api.jsonserve.com/DRp4Om")
@@ -15,6 +17,33 @@ function App() {
       });
   }, []);
 
+  const handleAddCard = () => {
+    // Generate a unique ID for the new card
+    const newCardId = Date.now();
+
+    // Create a new card object with recipient and message
+    const newCard = {
+      id: newCardId,
+      recipient: recipientInput,
+      message: defaultMessage,
+      sender: "The Mburu's", 
+    };
+
+    // Update the state with the new card
+    setCards(prevCards => [...prevCards, newCard]);
+
+    // Clear the input field
+    setRecipientInput('');
+  };
+
+  const handleDeleteCard = id => {
+    // Filter out the card with the given ID
+    const updatedCards = cards.filter(card => card.id !== id);
+
+    // Update the state with the updated card list
+    setCards(updatedCards);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -22,47 +51,40 @@ function App() {
         <p>
           HAPPY FATHERS DAY.
         </p>
+        <p>To all the fathers in this World</p>
         <a
           className="App-link"
           href="https://youtu.be/n8n-PDVYgu4"
           target="_blank"
           rel="noopener noreferrer"
         >
-          courtesy of you day
+          courtesy of your day see this video
         </a>
-        {cards && cards.map(card => (
-          <div key={card.id} className="card">
-            <h2>{card.recipient}</h2>
-            <p>{card.message}</p>
-            <p>From: {card.sender}</p>
-          </div>
-        ))}
+        <div className="card-form">
+          <input
+            type="text"
+            id="recipientInput"
+            placeholder="Enter recipient name"
+            value={recipientInput}
+            onChange={e => setRecipientInput(e.target.value)}
+          />
+          <button id="addButton" onClick={handleAddCard}>Add Card</button>
+        </div>
+        <div className="card-container">
+          {cards.map(card => (
+            <div key={card.id} className="card">
+              <h2>{card.recipient}</h2>
+              <p>{card.message}</p>
+              <p>From: {card.sender}</p>
+              <div className="card-actions">
+                <button onClick={() => handleDeleteCard(card.id)}>Delete</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
 }
-
-document.getElementById('searchButton').addEventListener('click', function() {
-  const recipientName = document.getElementById('recipientInput').value;
-
-  fetch("https://api.jsonserve.com/DRp4Om")
-    .then(response => response.json())
-    .then(data => {
-      const cards = data.cards;
-
-      // Find the card with matching recipient name
-      const matchingCard = cards.find(card => card.recipient === recipientName);
-
-      if (matchingCard) {
-        // Display the recipient's name
-        alert('Recipient Name: ' + matchingCard.recipient);
-      } else {
-        alert('No matching recipient found!');
-      }
-    })
-    .catch(error => {
-      console.log('Error:', error);
-    });
-});
 
 export default App;
